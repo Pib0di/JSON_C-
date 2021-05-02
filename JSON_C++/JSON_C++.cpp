29a52ae3592794e2b09c527fbc6fc7c0f9e6343d
Andrey <2001andru@mail.ru>
2021-05-03 08:08:33 +1000

    класс

--- JSON_C++/JSON_C++.cpp
+++ JSON_C++/JSON_C++.cpp
@@ -5,13 +5,47 @@
 #include <fstream>
 #include <iomanip>
 #include "json.hpp"
+#include <vector>
+#include <string>
 
 using json = nlohmann::json;
 using namespace std;
+typedef long long int ll_int;
+
+class Pharm
+{
+public:
+    Pharm() = default;
+    Pharm(string name, bool w, vector<ll_int> phone, int w_num, string remark) :
+        name(name), warehause(w), v_phone_num(phone), w_num(w_num), remark(remark)
+    {}
+
+private:
+    string name;
+    bool warehause = false; // наличие склада
+    vector<ll_int> v_phone_num; // номенр тел.
+    int w_num = 0; // номер базы
+    string remark = ""; // отзыв
+};
 
 int main()
 {
+    Pharm pharm();
     json j = "{ \"happy\": true, \"pi\": 3.141 }"_json;
+
+    vector<string> v_name = {"Скажем нет болезням", "Мое здоровье", "А. аптека"};
+    vector<bool> v_warehause = {true, false, false};
+    vector<vector<ll_int>> v_phone = { {89626766044, 89098367854, 94238764977}, {947586926409, 845357209521}, {924375627354} };
+    vector<int> v_w_num = {393, 254, 458};
+    vector<string> v_remark = {"Отличная аптека, низкие цены", "Не советую", "Закрыта!"};
+
+    for (int i = 0; i < v_name.size(); ++i)
+    {
+        
+    }
+
+
+    
 
 
 
